13d663f33315ab793c9ad6da25a24d25ac7c2bda
Flaque <econrad@zagmail.gonzaga.edu>
2018-12-25 12:19:00 -0700

    Add groupThoughtsByDays

--- src/thoughts.ts
+++ src/thoughts.ts
@@ -1,13 +1,54 @@
-import { CognitiveDistortion } from "./distortions";
+import distortions, { CognitiveDistortion } from "./distortions";
 
 export interface Thought {
   automaticThought: string;
   alternativeThought: string;
   cognitiveDistortions: CognitiveDistortion[];
   challenge: string;
+}
 
+export interface SavedThought extends Thought {
   // Only if saved
-  createdAt?: number;
-  updatedAt?: number;
-  uuid?: string;
+  createdAt: Date;
+  updatedAt: Date;
+  uuid: string;
+}
+
+export interface ThoughtGroup {
+  date: string;
+  thoughts: Thought[];
+}
+
+// This is a function instead of a constant to avoid some
+// REAL weird JS bugs
+export const newThought = (): Thought => {
+  return {
+    automaticThought: "",
+    cognitiveDistortions: distortions.map(({ label, slug }) => {
+      return { label, slug, selected: false };
+    }),
+    challenge: "",
+    alternativeThought: "",
+  };
+};
+
+export function groupThoughtsByDay(thoughts: SavedThought[]): ThoughtGroup[] {
+  const dates: string[] = [];
+  const groups: ThoughtGroup[] = [];
+
+  for (const thought of thoughts) {
+    const date = thought.createdAt.toDateString();
+    if (!dates.includes(date)) {
+      dates.push(date);
+      groups.push({
+        date,
+        thoughts: [thought],
+      });
+      continue;
+    }
+
+    groups[dates.length - 1].thoughts.push(thought);
+  }
+
+  return groups;
 }
