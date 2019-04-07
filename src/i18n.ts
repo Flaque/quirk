fd98a04c0f9c6a6d8c351e0f8d30c99c3fd732e9
devin <roche.devin@ymail.com>
2019-04-06 18:45:41 -0700

    remove console log

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,9 +3,6 @@ import i18n from 'i18n-js';
 import en from './locals/en.json'
 import fr from './locals/fr.json'
 
-console.log('Localization', Localization)
-console.log('i18n', i18n)
-
 i18n.fallbacks = true;
 i18n.translations = { fr, en };
 i18n.locale = Localization.locale;
