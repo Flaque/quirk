9946c9cc28b1362087b9f4138d94d9565afb0857
Ivo Damjanovic <ivo@damjanovic.it>
2019-04-07 20:26:30 +0200

    add german; proofreader wanted (#57)
    * add german
    
    * add german 2
    
    fix spelling and finish
    
    * Update i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,10 +3,10 @@ import i18n from 'i18n-js';
 import en from './locals/en.json'
 import es from './locals/es.json'
 import fr from './locals/fr.json'
-import pl from './locals/pl.json'
+import de from './locals/de.json'
 
 i18n.fallbacks = true;
-i18n.translations = { fr, en, pl, es };
+i18n.translations = { fr, en, pl, es, de };
 i18n.locale = Localization.locale;
 
 export default i18n;
