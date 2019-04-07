4dd4a9d4ce526eadec13230748d8a5458fc52460
Ivo Damjanovic <ivo@damjanovic.it>
2019-04-07 17:24:46 +0200

    add german

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,9 +2,10 @@ import { Localization } from 'expo';
 import i18n from 'i18n-js';
 import en from './locals/en.json'
 import fr from './locals/fr.json'
+import fr from './locals/de.json'
 
 i18n.fallbacks = true;
-i18n.translations = { fr, en };
+i18n.translations = { de, fr, en };
 i18n.locale = Localization.locale;
 
 export default i18n;
