31caad8d209d6b3d517319eb8284591f02269490
Flaque <econrad@zagmail.gonzaga.edu>
2019-04-07 11:32:16 -0700

    Fix quotes and my silly merge

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,9 +1,10 @@
-import { Localization } from 'expo';
-import i18n from 'i18n-js';
-import en from './locals/en.json'
-import es from './locals/es.json'
-import fr from './locals/fr.json'
-import de from './locals/de.json'
+import { Localization } from "expo";
+import i18n from "i18n-js";
+import en from "./locals/en.json";
+import es from "./locals/es.json";
+import fr from "./locals/fr.json";
+import de from "./locals/de.json";
+import pl from "./locals/pl.json";
 
 i18n.fallbacks = true;
 i18n.translations = { fr, en, pl, es, de };
