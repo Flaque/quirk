007456e38e2b6b80a3e061c0f9631cc35de3cd6f
Karolina <k.a.wierbol@gmail.com>
2019-04-07 20:23:42 +0200

    Add Polish language (#58)

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,11 +1,12 @@
 import { Localization } from 'expo';
 import i18n from 'i18n-js';
 import en from './locals/en.json'
-import fr from './locals/fr.json'
 import es from './locals/es.json'
+import fr from './locals/fr.json'
+import pl from './locals/pl.json'
 
 i18n.fallbacks = true;
-i18n.translations = { fr, en, es };
+i18n.translations = { fr, en, pl, es };
 i18n.locale = Localization.locale;
 
 export default i18n;
