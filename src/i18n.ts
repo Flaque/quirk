0d1c5f4ffac64b90712a2b7a6611e6248f67fb72
Ivo Damjanovic <ivo@damjanovic.it>
2019-04-07 18:37:20 +0200

    Update i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,7 +2,7 @@ import { Localization } from 'expo';
 import i18n from 'i18n-js';
 import en from './locals/en.json'
 import fr from './locals/fr.json'
-import fr from './locals/de.json'
+import de from './locals/de.json'
 
 i18n.fallbacks = true;
 i18n.translations = { de, fr, en };
