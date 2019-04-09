101f8f00d6fb60c716630c8b27b801c7abf2f2db
Flaque <econrad@zagmail.gonzaga.edu>
2019-04-08 20:22:05 -0700

    fix expo

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,7 @@
-import { Localization } from 'expo';
-import i18n from 'i18n-js';
-import en from './locals/en.json'
-import fr from './locals/fr.json'
+import { Localization } from "expo";
+import i18n from "i18n-js";
+import en from "./locals/en.json";
+import fr from "./locals/fr.json";
 
 i18n.fallbacks = true;
 i18n.translations = { fr, en };
