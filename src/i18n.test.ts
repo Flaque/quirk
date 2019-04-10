b85b3de5ed76060d342a90f52af4b5d39d493448
Brian Kung <brian@callmekung.com>
2019-04-10 13:00:44 -0500

    Test English keys are a subset of the localization keys

--- src/i18n.test.ts
+++ src/i18n.test.ts
@@ -1,4 +1,4 @@
-import { getKeysOnly } from './test_support'
+import { getKeysOnly } from './test_support';
 
 import en from './locals/en.json';
 import es from './locals/es.json';
@@ -6,14 +6,14 @@ import de from './locals/de.json';
 import pl from './locals/pl.json';
 
 test("Spanish localization keys match English localization keys", () => {
-  expect(getKeysOnly(es)).toEqual(getKeysOnly(en));
+  expect(getKeysOnly(es)).toMatchObject(getKeysOnly(en));
 });
 
 test("German localization keys match English localization keys", () => {
-  expect(getKeysOnly(de)).toEqual(getKeysOnly(en));
+  expect(getKeysOnly(de)).toMatchObject(getKeysOnly(en));
 });
 
 test("Polish localization keys match English localization keys", () => {
-  expect(getKeysOnly(pl)).toEqual(getKeysOnly(en));
+  expect(getKeysOnly(pl)).toMatchObject(getKeysOnly(en));
 });
 
