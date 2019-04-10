398b0e671e7d1dcd24457f55891424e14af9b162
Jos512 <user@server.fake>
2019-04-10 15:09:39 +0200

    Fix conflicts with src/i18n.ts file

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,11 +1,13 @@
-import { Localization } from 'expo';
-import i18n from 'i18n-js';
-import en from './locals/en.json'
-import fr from './locals/fr.json'
-import nl from './locals/nl_NL.json'
+export default i18n;
+
+import { Localization } from "expo";
+import i18n from "i18n-js";
+import en from "./locals/en.json";
+import fr from "./locals/fr.json";
+import nl from "./locals/nl_NL.json";
 
 i18n.fallbacks = true;
-i18n.translations = { fr, en, nl };
+i18n.translations = { en, fr, nl };
 i18n.locale = Localization.locale;
 
 export default i18n;
