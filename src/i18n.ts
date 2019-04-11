fe2bd5dc05df49ba5ca7bde77793b72799c45c80
Jos512 <Jos512@users.noreply.github.com>
2019-04-11 05:19:33 +0200

    Add Dutch translation (#69)
    * Add Dutch translation
    
    * Reword two cognitieve distortions for a better translation
    
    * Fix conflicts with src/i18n.ts file

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,3 +1,5 @@
+export default i18n;
+
 import { Localization } from "expo";
 import i18n from "i18n-js";
 import en from "./locals/en.json";
@@ -6,10 +8,11 @@ import ko from "./locals/ko.json";
 import es from "./locals/es.json";
 import de from "./locals/de.json";
 import pl from "./locals/pl.json";
+import nl from "./locals/nl_NL.json";
 import fi from "./locals/fi.json";
 
 i18n.fallbacks = true;
-i18n.translations = { fr, en, ko, pl, es, de, fi };
+i18n.translations = { fr, en, ko, pl, es, de, fi, nl };
 i18n.locale = Localization.locale;
 
 export default i18n;
