0648239adabfdad455915cde3f620a965aa62f0f
jay park <jaypark@gmail.com>
2019-04-11 11:52:48 +0900

    Add Korea (#67)

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,13 +1,14 @@
 import { Localization } from "expo";
 import i18n from "i18n-js";
 import en from "./locals/en.json";
-import es from "./locals/es.json";
 import fr from "./locals/fr.json";
+import ko from "./locals/ko.json";
+import es from "./locals/es.json";
 import de from "./locals/de.json";
 import pl from "./locals/pl.json";
 
 i18n.fallbacks = true;
-i18n.translations = { fr, en, pl, es, de };
+i18n.translations = { fr, en, ko, pl, es, de };
 i18n.locale = Localization.locale;
 
 export default i18n;
