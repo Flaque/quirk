097286c86ec6ad89c4c6e3340b82647023c4bd8a
Flaque <econrad@zagmail.gonzaga.edu>
2019-04-10 19:57:55 -0700

    merge finish

--- src/i18n.ts
+++ src/i18n.ts
@@ -6,9 +6,10 @@ import ko from "./locals/ko.json";
 import es from "./locals/es.json";
 import de from "./locals/de.json";
 import pl from "./locals/pl.json";
+import fi from "./locals/fi.json";
 
 i18n.fallbacks = true;
-i18n.translations = { fr, en, ko, pl, es, de };
+i18n.translations = { fr, en, ko, pl, es, de, fi };
 i18n.locale = Localization.locale;
 
 export default i18n;
