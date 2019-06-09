76a9caa2b1111733a47c043527f7931c03e667bf
Flaque <evanjamesconrad@gmail.com>
2019-06-09 13:48:04 -0700

    Add grandfather

---
+++ src/history/grandfatherstore.ts
@@ -0,0 +1,46 @@
+/*
+			
+              ,-----.
+            W/,-. ,-.\W
+            ()>a   a<()
+            (.--(_)--.)
+          ,'/.-'\_/`-.\`.
+        ,' /    `-'    \ `.
+       /   \           /   \
+      /     `.       ,'     \
+     /    /   `-._.-'   \    \
+   ,-`-._/|     |=|o    |\_.-<
+  <,--.)  |_____| |o____|  )_ \
+   `-)|    |//   _   \\|     )/
+     ||    |'    |    `|
+     ||    |     |     |
+     ||    (    )|(    )
+     ||    |     |     |
+     ||    |     |     |
+     ||    |_.--.|.--._|
+     ||     /'""| |""`\
+     []     `===' `==='  hjw
+
+*/
+import { AsyncStorage } from "react-native";
+
+const FREE_SUBSCRIPTION_KEY = "@Grandfathered:free-subscription";
+
+export async function isGrandfatheredIntoFreeSubscription(): Promise<boolean> {
+  try {
+    const value = await AsyncStorage.getItem(FREE_SUBSCRIPTION_KEY);
+    return !!value;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+}
+
+// TODO: Remove
+export async function grandfatherUserIntoFreeSubscription() {
+  try {
+    await AsyncStorage.setItem(FREE_SUBSCRIPTION_KEY, "true");
+  } catch (err) {
+    console.error(err);
+  }
+}
