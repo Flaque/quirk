478a46c4c950c8d99546f645b875679d0e14e08c
Flaque <evanjamesconrad@gmail.com>
2019-06-18 10:43:33 -0700

    add a whole buncha stuff

--- android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -25,10 +25,12 @@ public class AppConstants {
     List<Constants.EmbeddedResponse> embeddedResponses = new ArrayList<>();
 
     
+        
+        
         // ADD EMBEDDED RESPONSES HERE
         // START EMBEDDED RESPONSES
         embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@flaque/quirk", "assets://shell-app-manifest.json", "application/json"));
-        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40flaque%2Fquirk%2F2.0.0%2Fd96a11b0d90d977a1d902c7bba84ff36-31.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40flaque%2Fquirk%2F2.0.1%2Fcc4869e92ee495c88d44b84ddf3cd06f-31.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
         // END EMBEDDED RESPONSES
     EMBEDDED_RESPONSES = embeddedResponses;
   }
--- src/subscriptions/index.ts
+++ src/subscriptions/index.ts
@@ -38,41 +38,43 @@ export async function requiresPayment(): Promise<boolean> {
   // Step 1: Check local storage first
   if (await subscriptionStore.hasValidSubscription()) {
     stats.subscriptionVerified("cache");
+    console.log("Has subscription in cache");
     return false;
   }
 
   // Step 2: Can they connect to the stores?
   const canMakePayments = await InAppPurchases.initConnection();
   if (!canMakePayments) {
+    console.log("Can't make payments");
     stats.subscriptionGivenForFreeDueToError();
     return false; // They get a free ride if there's a problem here
   }
 
   // Step 3: Check online if they've ever purchased anything
-  const availablePurchases =
-    (await InAppPurchases.getAvailablePurchases()) || [];
-  const historicalPurchases = (await InAppPurchases.getPurchaseHistory()) || [];
-  const purchases = [].concat(availablePurchases, historicalPurchases);
+  const purchases = (await InAppPurchases.getAvailablePurchases()) || [];
   if (!purchases || purchases.length === 0) {
+    console.log("Never purchased");
     stats.subscriptionUnverified("never-bought");
     return true;
   }
 
-  // Step 4: Check online if their most recent purchase is still valid
-  const purchaseDate = await getMostRecentOnlinePurchaseDate(purchases);
-  const expirationDate = dayjs.unix(purchaseDate).add(1, "month");
+  // // Step 4: Check online if their most recent purchase is still valid
+  // const purchaseDate = await getMostRecentOnlinePurchaseDate(purchases);
+  // const expirationDate = dayjs.unix(purchaseDate).add(1, "month");
 
-  // Is today after the expiration date?
-  const isExpired = dayjs().isAfter(expirationDate);
+  // // Is today after the expiration date?
+  // const isExpired = dayjs().isAfter(expirationDate);
 
-  // Cache this so we don't have to look it up again
-  if (!isExpired) {
-    subscriptionStore.storeExpirationDate(expirationDate.unix());
-    stats.subscriptionVerified("online");
-    return false;
-  } else {
-    stats.subscriptionUnverified("expired");
-  }
+  // // Cache this so we don't have to look it up again
+  // if (!isExpired) {
+  //   console.log("Is Expired");
+  //   subscriptionStore.storeExpirationDate(expirationDate.unix());
+  //   stats.subscriptionVerified("online");
+  //   return false;
+  // } else {
+  //   console.log("Expired");
+  //   stats.subscriptionUnverified("expired");
+  // }
 
   return true;
 }
