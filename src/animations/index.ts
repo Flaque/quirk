bbddf9954cee858216329ef47968053630fd2cc0
Flaque <evanjamesconrad@gmail.com>
2019-07-10 14:56:02 -0700

    WIP

--- src/animations/index.ts
+++ src/animations/index.ts
@@ -4,3 +4,9 @@ export const FadesIn = posed.View({
   visible: { opacity: 1 },
   hidden: { opacity: 0 },
 });
+
+export const BouncyBigOnActive = posed.View({
+  active: { scale: 1, transition: { type: "spring", stiffness: 200 } },
+  inactive: { scale: 0.5 },
+  upcoming: { y: -24 },
+});
--- src/lock/LockScreen.tsx
+++ src/lock/LockScreen.tsx
@@ -4,17 +4,23 @@ import {
   NavigationState,
   NavigationAction,
 } from "react-navigation";
-import { View, StatusBar } from "react-native";
-import { Container, Row, Header, GhostButton } from "../ui";
+import { StatusBar } from "react-native";
+import { Container, Row, GhostButton, Header } from "../ui";
 import theme from "../theme";
 import { Constants } from "expo";
-import posed from "react-native-pose";
+import {
+  FadesIn,
+  BlueOnActive,
+  BigOnActive,
+  BiggleOnActive,
+  BouncyBigOnActive,
+} from "../animations";
 
 interface ScreenProps {
   navigation: NavigationScreenProp<NavigationState, NavigationAction>;
 }
 
-const KeypadButton = ({ title }) => (
+const KeypadButton = ({ title, onPress }) => (
   <GhostButton
     title={title}
     borderColor={theme.gray}
@@ -25,100 +31,156 @@ const KeypadButton = ({ title }) => (
     style={{
       backgroundColor: "white",
     }}
+    onPress={onPress}
   />
 );
 
-const PopsUp = posed.View({
-  full: { height: 380, paddingTop: 18, paddingBottom: 18 },
-  peak: {
-    height: 156,
-    paddingTop: 18,
-    paddingBottom: 18,
-    transition: { type: "spring", stiffness: 150 },
-  },
-  hidden: { height: 0, paddingTop: 0, paddingBottom: 0 },
-});
+const Notifier = ({ isActive }) => (
+  <BouncyBigOnActive
+    style={{
+      width: 32,
+      height: 32,
+      borderRadius: 32,
+      backgroundColor: theme.pink,
+    }}
+    pose={isActive ? "active" : "inactive"}
+  />
+);
 
 const BUTTON_SIZE = 96;
 
-export default class extends React.Component<ScreenProps> {
+export default class extends React.Component<
+  ScreenProps,
+  {
+    isReady: boolean;
+    code: string;
+  }
+> {
   static navigationOptions = {
     header: null,
   };
 
   state = {
-    view: "hidden",
+    isReady: false,
+    code: "",
   };
 
   componentDidMount() {
     setTimeout(() => {
-      this.setState({ view: "peak" });
-    }, 350);
+      this.setState({ isReady: true });
+    }, 100);
   }
 
+  onEnterCode = () => {
+    this.setState(prevState => {
+      return {
+        ...prevState,
+        code: prevState.code + "0",
+      };
+    });
+  };
+
   render() {
+    const { code } = this.state;
     return (
-      <View
+      <FadesIn
         style={{
-          backgroundColor: theme.pink,
+          backgroundColor: theme.offwhite,
           height: "100%",
         }}
+        pose={this.state.isReady ? "visible" : "hidden"}
       >
         <StatusBar barStyle="dark-content" />
-
-        <PopsUp>
-          <Container
+        <Container
+          style={{
+            flex: 1,
+            paddingLeft: 12,
+            paddingRight: 12,
+            paddingTop: 24,
+            marginTop: Constants.statusBarHeight,
+            backgroundColor: theme.offwhite,
+            justifyContent: "center",
+          }}
+        >
+          <Row
             style={{
-              flex: 1,
-              paddingLeft: 0,
-              paddingRight: 0,
-              paddingTop: 24,
-              marginTop: Constants.statusBarHeight,
+              alignSelf: "center",
             }}
           >
-            <Row
+            <Header
               style={{
-                justifyContent: "space-evenly",
-                marginBottom: 12,
+                fontSize: 32,
+                marginBottom: 24,
               }}
             >
-              <KeypadButton title="1" />
-              <KeypadButton title="2" />
-              <KeypadButton title="3" />
-            </Row>
+              Enter your passcode.
+            </Header>
+          </Row>
+        </Container>
 
-            <Row
-              style={{
-                justifyContent: "space-evenly",
-                marginBottom: 12,
-              }}
-            >
-              <KeypadButton title="4" />
-              <KeypadButton title="5" />
-              <KeypadButton title="6" />
-            </Row>
+        <Container
+          style={{
+            flex: 1,
+            paddingLeft: 12,
+            paddingRight: 12,
+            paddingTop: 24,
+            backgroundColor: "white",
+          }}
+        >
+          <Row
+            style={{
+              marginLeft: 48,
+              marginRight: 48,
+              marginBottom: 24,
+            }}
+          >
+            <Notifier isActive={code.length >= 1} />
+            <Notifier isActive={code.length >= 2} />
+            <Notifier isActive={code.length >= 3} />
+            <Notifier isActive={code.length >= 4} />
+          </Row>
+          <Row
+            style={{
+              justifyContent: "space-evenly",
+              marginBottom: 12,
+            }}
+          >
+            <KeypadButton title="1" onPress={this.onEnterCode} />
+            <KeypadButton title="2" onPress={this.onEnterCode} />
+            <KeypadButton title="3" onPress={this.onEnterCode} />
+          </Row>
 
-            <Row
-              style={{
-                justifyContent: "space-evenly",
-                marginBottom: 12,
-              }}
-            >
-              <KeypadButton title="7" />
-              <KeypadButton title="8" />
-              <KeypadButton title="9" />
-            </Row>
+          <Row
+            style={{
+              justifyContent: "space-evenly",
+              marginBottom: 12,
+            }}
+          >
+            <KeypadButton title="4" onPress={this.onEnterCode} />
+            <KeypadButton title="5" onPress={this.onEnterCode} />
+            <KeypadButton title="6" onPress={this.onEnterCode} />
+          </Row>
 
-            <Row
-              style={{
-                justifyContent: "space-evenly",
-              }}
-            >
-              <KeypadButton title="0" />
-            </Row>
-          </Container>
-        </PopsUp>
-      </View>
+          <Row
+            style={{
+              justifyContent: "space-evenly",
+              marginBottom: 12,
+            }}
+          >
+            <KeypadButton title="7" onPress={this.onEnterCode} />
+            <KeypadButton title="8" onPress={this.onEnterCode} />
+            <KeypadButton title="9" onPress={this.onEnterCode} />
+          </Row>
+
+          <Row
+            style={{
+              justifyContent: "space-evenly",
+            }}
+          >
+            <KeypadButton title="0" onPress={this.onEnterCode} />
+          </Row>
+        </Container>
+      </FadesIn>
     );
   }
 }
