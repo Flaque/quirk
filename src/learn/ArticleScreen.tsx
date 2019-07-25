02d4b40c1fb56fc5d90957b41ffab7617ffbe671
Flaque <evanjamesconrad@gmail.com>
2019-07-24 20:37:03 -0700

    Add spinner

--- src/learn/ArticleScreen.tsx
+++ src/learn/ArticleScreen.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ScreenProps from "../ScreenProps";
 import Constants from "expo-constants";
-import { WebView, View } from "react-native";
+import { WebView, View, ActivityIndicator, Dimensions } from "react-native";
 import { GhostButton } from "../ui";
 import { INDEX_LEARN_SCREEN } from "./screens";
 import theme from "../theme";
@@ -13,17 +13,19 @@ export default class ArticleScreen extends React.Component<ScreenProps> {
 
   state = {
     url: "",
+    isLoading: true,
   };
 
   constructor(props) {
     super(props);
+
+    // Having both of these hack-fixes a bug where the state gets mixed up
     this.props.navigation.addListener("willFocus", args => {
       const url = get(args, "state.params.url");
       this.setState({
         url,
       });
     });
-
     this.props.navigation.addListener("didFocus", args => {
       const url = get(args, "state.params.url");
       this.setState({
@@ -33,7 +35,6 @@ export default class ArticleScreen extends React.Component<ScreenProps> {
   }
 
   render() {
-    console.log("URL:", this.state.url);
     return (
       <View
         style={{
@@ -61,12 +62,29 @@ export default class ArticleScreen extends React.Component<ScreenProps> {
         </View>
 
         <WebView
+          onLoad={() => {
+            this.setState({
+              isLoading: false,
+            });
+          }}
           style={{
             flex: 1,
             marginHorizontal: 24,
           }}
+          javaScriptEnabled={false}
           source={{ uri: this.state.url }}
         />
+
+        {this.state.isLoading && (
+          <ActivityIndicator
+            style={{
+              position: "absolute",
+              top: Dimensions.get("screen").height / 2,
+              left: Dimensions.get("screen").width / 2,
+            }}
+            size="large"
+          />
+        )}
       </View>
     );
   }
