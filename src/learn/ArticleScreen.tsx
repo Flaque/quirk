312c6fbc84176b2d8c66336293d3641446d1707b
Flaque <evanjamesconrad@gmail.com>
2019-07-24 19:59:06 -0700

    Add better help screen

--- src/learn/ArticleScreen.tsx
+++ src/learn/ArticleScreen.tsx
@@ -1,13 +1,73 @@
 import React from "react";
 import ScreenProps from "../ScreenProps";
-import { Container } from "../ui";
-
+import Constants from "expo-constants";
+import { WebView, View } from "react-native";
+import { GhostButton } from "../ui";
+import { INDEX_LEARN_SCREEN } from "./screens";
+import theme from "../theme";
+import { get } from "lodash";
 export default class ArticleScreen extends React.Component<ScreenProps> {
   static navigationOptions = {
     header: null,
   };
 
+  state = {
+    url: "",
+  };
+
+  constructor(props) {
+    super(props);
+    this.props.navigation.addListener("willFocus", args => {
+      const url = get(args, "state.params.url");
+      this.setState({
+        url,
+      });
+    });
+
+    this.props.navigation.addListener("didFocus", args => {
+      const url = get(args, "state.params.url");
+      this.setState({
+        url,
+      });
+    });
+  }
+
   render() {
-    return <Container>hix</Container>;
+    console.log("URL:", this.state.url);
+    return (
+      <View
+        style={{
+          marginTop: Constants.statusBarHeight + 24,
+          flex: 1,
+        }}
+      >
+        <View
+          style={{
+            paddingBottom: 12,
+            paddingHorizontal: 24,
+            justifyContent: "space-between",
+            alignItems: "center",
+            flexDirection: "row",
+          }}
+        >
+          <GhostButton
+            title="Back to list"
+            borderColor={theme.lightGray}
+            textColor={theme.lightText}
+            onPress={() => {
+              this.props.navigation.navigate(INDEX_LEARN_SCREEN);
+            }}
+          />
+        </View>
+
+        <WebView
+          style={{
+            flex: 1,
+            marginHorizontal: 24,
+          }}
+          source={{ uri: this.state.url }}
+        />
+      </View>
+    );
   }
 }
--- src/learn/IndexLearnScreen.tsx
+++ src/learn/IndexLearnScreen.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Text, Image, View, ScrollView, TouchableOpacity } from "react-native";
+import { View, ScrollView, TouchableOpacity } from "react-native";
 import ScreenProps from "../ScreenProps";
-import { Container, Row, SubHeader, MediumHeader, HintHeader } from "../ui";
+import { Container, MediumHeader, HintHeader } from "../ui";
 import Constants from "expo-constants";
 import theme from "../theme";
 import {
@@ -19,8 +19,20 @@ import {
   ShouldStatementsImage,
 } from "./images";
 import { TAB_BAR_HEIGHT } from "../tabbar/TabBar";
+import { ARTICLE_SCREEN } from "./screens";
 
-const Article = ({ title, subheader, HeroImage }) => (
+const Article = ({
+  title,
+  subheader,
+  HeroImage,
+  url,
+  navigation,
+}: ScreenProps & {
+  title: string;
+  subheader: string;
+  HeroImage: any;
+  url: string;
+}) => (
   <View
     style={{
       alignSelf: "center",
@@ -29,7 +41,13 @@ const Article = ({ title, subheader, HeroImage }) => (
   >
     <MediumHeader>{title}</MediumHeader>
     <HintHeader>{subheader}</HintHeader>
-    <TouchableOpacity>
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate(ARTICLE_SCREEN, {
+          url,
+        });
+      }}
+    >
       <HeroImage />
     </TouchableOpacity>
   </View>
@@ -57,61 +75,85 @@ export default class IndexLearnScreen extends React.Component<ScreenProps> {
             title="All or Nothing Thinking"
             subheader={"1 min read - Cognitive Distortion"}
             HeroImage={AllOrNothingThinkingImage}
+            navigation={this.props.navigation}
+            url={"https://embedded.quirk.fyi/all-or-nothing"}
           />
           <Article
             title="Catastrophizing"
             subheader={"1 min read - Cognitive Distortion"}
             HeroImage={CatastrophizingImage}
+            navigation={this.props.navigation}
+            url={"https://embedded.quirk.fyi/catastrophizing"}
           />
           <Article
             title="Emotional Reasoning"
             subheader={"1 min read - Cognitive Distortion"}
             HeroImage={EmotionalReasoningImage}
+            navigation={this.props.navigation}
+            url={"https://embedded.quirk.fyi/emotional-reasoning"}
           />
           <Article
             title="Fortune Telling"
             subheader={"1 min read - Cognitive Distortion"}
             HeroImage={FortuneTellingImage}
+            navigation={this.props.navigation}
+            url={"https://embedded.quirk.fyi/fortune-telling"}
           />
           <Article
             title="Labeling"
             subheader={"1 min read - Cognitive Distortion"}
             HeroImage={LabelingImage}
+            navigation={this.props.navigation}
+            url={"https://embedded.quirk.fyi/labeling"}
           />
           <Article
             title="Magnification of the Negative"
             subheader={"1 min read - Cognitive Distortion"}
             HeroImage={MaxNegativeImage}
+            navigation={this.props.navigation}
+            url={"https://embedded.quirk.fyi/magnification-of-the-negative"}
           />
           <Article
             title="Mind Reading"
             subheader={"1 min read - Cognitive Distortion"}
             HeroImage={MindReadingImage}
+            navigation={this.props.navigation}
+            url={"https://embedded.quirk.fyi/mind-reading"}
           />
           <Article
             title="Minimization of the Positive"
             subheader={"1 min read - Cognitive Distortion"}
             HeroImage={MinPositiveImage}
+            navigation={this.props.navigation}
+            url={"https://embedded.quirk.fyi/minimization-of-the-positive"}
           />
           <Article
             title="Other Blaming"
             subheader={"1 min read - Cognitive Distortion"}
             HeroImage={OtherBlamingImage}
+            navigation={this.props.navigation}
+            url={"https://embedded.quirk.fyi/other-blaming"}
           />
           <Article
             title="Overgeneralization"
             subheader={"1 min read - Cognitive Distortion"}
             HeroImage={OverGeneralizationImage}
+            navigation={this.props.navigation}
+            url={"https://embedded.quirk.fyi/overgeneralization"}
           />
           <Article
             title="Self Blaming"
             subheader={"1 min read - Cognitive Distortion"}
             HeroImage={SelfBlamingImage}
+            navigation={this.props.navigation}
+            url={"https://embedded.quirk.fyi/self-blaming"}
           />
           <Article
             title="Should Statements"
             subheader={"1 min read - Cognitive Distortion"}
             HeroImage={ShouldStatementsImage}
+            navigation={this.props.navigation}
+            url={"https://embedded.quirk.fyi/should-statements"}
           />
         </Container>
       </ScrollView>
