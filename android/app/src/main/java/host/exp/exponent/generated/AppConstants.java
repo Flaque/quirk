d5e5f333c8766f90f47e3d5966d6ce360e41a356
Flaque <evanjamesconrad@gmail.com>
2019-08-10 13:51:18 -0700

    release ios version

--- android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -133,10 +133,11 @@ public class AppConstants {
         
         
         
+        
         // ADD EMBEDDED RESPONSES HERE
         // START EMBEDDED RESPONSES
         embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@flaque/quirk", "assets://shell-app-manifest.json", "application/json"));
-        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40flaque%2Fquirk%2F2.3.1%2F97a51a482ba1210341ad6d1e302efb4c-33.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40flaque%2Fquirk%2F2.3.1%2F50d23cdb611aacd10119d58d1eb880ea-33.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
         // END EMBEDDED RESPONSES
     EMBEDDED_RESPONSES = embeddedResponses;
   }
