6fb9c0c855fe8aa55e305c5744f7a830ca73c464
Beringer Zsolt <51266806+zsolt-beringer@users.noreply.github.com>
2019-09-08 09:23:43 +0200

    Update i18n.ts with Hungarian translation

--- src/i18n.ts
+++ src/i18n.ts
@@ -16,6 +16,7 @@ import ptBR from "./locals/pt-br.json";
 import nb from "./locals/nb.json";
 import sv from "./locals/sv.json";
 import ro from "./locals/ro.json";
+import hu from "./locals/hu.json";
 
 i18n.fallbacks = true;
 i18n.translations = {
@@ -35,6 +36,7 @@ i18n.translations = {
   nb,
   sv,
   ro,
+  hu,
 };
 i18n.locale = Localization.locale;
 
