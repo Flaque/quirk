d433a5d8f1612857cf8bf91e6985e48ea0581099
Flaque <evanjamesconrad@gmail.com>
2019-09-26 19:07:07 -0700

    Update note

--- src/articles/content/pulse.ts
+++ src/articles/content/pulse.ts
@@ -4,18 +4,24 @@ export const pages = [
   // One
   `# What's Awareness? 
 
-The Awareness graph is a rough measure of how likely you are to challenge a negative thought. Often, automatic negative thoughts consume your thinking before you even become aware of them. With practice, you can become more aware in the moment. To do so, you'll need to build a habit that you may not be used to. To help, the Awareness chart will show you the strength of your habit over time. 
+  Challenging thoughts in the moment is hard. It requires an awareness of your thoughts that takes practice to build up. 
+  
+  To help, we've created the Awareness score. It shows you the rough likelihood that you'll challenge any negative thought in the moment. You can also think about it as the "strength" of your thought-challenging habit. 
+
+  This really is a _skill_ you have to build up and the Awareness graph will help you see your progress over time.
   `,
 
   // Two
   `# How Awareness works 
 
-When you challenge thoughts, make predictions, or follow-up later, your awareness will go up since CBT is likely top of mind. Like any habit, Awareness will naturally drop over time. This is natural and just part of the experience. 
+When you challenge thoughts, make predictions, or follow-up later, your awareness score will go up since CBT is likely top of mind. 
+
+Like any skill, Awareness will naturally drop over time without regular practice. This is natural and just part of the experience. 
 `,
 ];
 
 export const title = "Awareness";
-export const description = "Charting your progress over time.";
+export const description = "See your progress over time.";
 
 const content: Content = {
   pages,
