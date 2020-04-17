0863fa80fa0f6ae15ee55bd81f82b74c82285fa4
Ninu1234 <poonam.khichar1@gmail.com>
2020-04-17 16:23:10 +0530

    change header color

--- src/lock/LockScreen.tsx
+++ src/lock/LockScreen.tsx
@@ -202,7 +202,7 @@ export default class extends React.Component<
             <Header
               style={{
                 fontSize: 32,
-                color: "white",
+                color: "green",
                 marginHorizontal: 24,
                 textAlign: "center",
               }}
