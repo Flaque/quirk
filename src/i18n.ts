8733d001555c7908afda033f0e65204feb6ccf9f
YashikaSharmaa <60700359+YashikaSharmaa@users.noreply.github.com>
2020-08-05 09:42:29 +0530

    Add Hindi language
    Translated this app to Hindi Language.
    Translation of the description of the app for stores:      क्वर्क संज्ञानात्मक व्यवहार थेरेपी (सीबीटी) के लिए एक उपकरण है। सीबीटी अवसाद, चिंता, आतंक विकार और कई अन्य मानसिक स्वास्थ्य समस्याओं के लिए सबसे प्रभावी और व्यापक रूप से निर्धारित उपचारों में से एक है।
    क्वर्क आपको "स्वचालित विचारों" को रिकॉर्ड करने में मदद करता है, उन्हें चुनौती देता है, और फिर एक वैकल्पिक विचार के साथ अपने मस्तिष्क को प्रशिक्षित करता है। ऐप GPL के तहत खुला स्रोत है। आप Github पर कोड: https://github.com/Flaque/quirk पर पा सकते हैं
     translate of the keywords (required by iOS localization):     
    चिंता, अवसाद, संज्ञानात्मक व्यवहार थेरेपी, सीबीटी, आतंक

--- src/i18n.ts
+++ src/i18n.ts
@@ -16,6 +16,7 @@ import ptBR from "./locals/pt-br.json";
 import nb from "./locals/nb.json";
 import sv from "./locals/sv.json";
 import ro from "./locals/ro.json";
+import hi from "./locals/hi.json";
 
 i18n.fallbacks = true;
 i18n.translations = {
@@ -35,6 +36,7 @@ i18n.translations = {
   nb,
   sv,
   ro,
+  hi
 };
 i18n.locale = Localization.locale;
 
