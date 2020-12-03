b285b4e0350abe1d4b887023543cfca58cb47d4e
vedika28 <vedika.agarwal287@gmail.com>
2020-12-03 19:26:17 +0530

    changed the styling

--- src/textInputStyle.tsx
+++ src/textInputStyle.tsx
@@ -4,14 +4,14 @@ import { Dimensions, TextInput as ReactNativeTextInput } from "react-native";
 
 export const textInputStyle = {
   height: Dimensions.get("screen").height * 0.15,
-  backgroundColor: "white",
-  padding: 12,
-  paddingTop: 14,
-  borderRadius: 8,
-  fontSize: 16,
+  backgroundColor: "rgb(111,212,227))",
+  padding: 15,
+  paddingTop: 17,
+  borderRadius: 6,
+  fontSize: 18,
   borderColor: theme.lightGray,
-  borderWidth: 1,
-  borderBottomWidth: 2,
+  borderWidth: 3,
+  borderBottomWidth: 5,
   color: theme.darkText,
   textAlignVertical: "top",
 };
