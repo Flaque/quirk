11303429276c810a06d7ba7ce3bea03100bccd73
Abhigyan Ranjan <71014464+AbhigyanRanjan0505@users.noreply.github.com>
2021-01-10 21:21:24 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -14,7 +14,6 @@ import SettingScreen from "./src/SettingsScreen";
 import withErrorBoundary from "./src/sentry/withErrorBoundary";
 import PaymentScreen from "./src/payments/PaymentScreen";
 import LockScreen from "./src/lock/LockScreen";
-import MainScreen from "./src/main";
 import React from "react";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import TabBar from "./src/tabbar/TabBar";
@@ -25,8 +24,8 @@ import { setCustomText } from "react-native-global-props";
 import { Platform } from "react-native";
 import SupportScreen from "./src/payments/SupportScreen";
 import MarkdownArticleScreen from "./src/articles/MarkdownArticleScreen";
+import { INDEX_LEARN_SCREEN } from "./src/learn/screens";
 
-// Fixes a bug on OnePlus phones which have some buggy font by default
 if (Platform.OS === "android") {
   setCustomText({
     style: {
@@ -37,19 +36,20 @@ if (Platform.OS === "android") {
 
 const App = createBottomTabNavigator(
   {
-    [MAIN_SCREEN]: MainScreen,
-    [SETTING_SCREEN]: SettingScreen,
-    [EXPLANATION_SCREEN]: IndexLearnScreen,
-    [PAYMENT_SCREEN]: PaymentScreen,
-    [LOCK_SCREEN]: LockScreen,
-    [CBT_ON_BOARDING_SCREEN]: OnboardingScreen,
-    [CHECKUP_SCREEN]: CheckupScreen,
-    [SUPPORT_SCREEN]: SupportScreen,
-    [MARKDOWN_ARTICLE_SCREEN]: MarkdownArticleScreen,
+    MainScreen: { screen: MAIN_SCREEN },
+    SettingScreen: { screen: SETTING_SCREEN },
+    IndexLearnScreen: { screen: INDEX_LEARN_SCREEN },
+    PaymentScreen: { screen: PAYMENT_SCREEN },
+    LockScreen: { screen: LOCK_SCREEN },
+    ExplanationScreen: { screen: EXPLANATION_SCREEN },
+    OnboardingScreen: { screen: CBT_ON_BOARDING_SCREEN },
+    CheckupScreen: { screen: CHECKUP_SCREEN },
+    SupportScreen: { screen: SUPPORT_SCREEN },
+    MarkdownArticleScreen: { screen: MARKDOWN_ARTICLE_SCREEN },
   },
   {
     initialRouteName: PAYMENT_SCREEN,
-    tabBarComponent: props => {
+    tabBarComponent: (props) => {
       return <TabBar {...props} />;
     },
   }
