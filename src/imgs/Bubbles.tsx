18e7a33d80cc80e95c2d47c0169a06e4bf526bb1
yasamanfaizi <yasamanfeizizadeh@gmail.com>
2021-04-01 17:23:54 -0400

    Hello

--- src/imgs/Bubbles.tsx
+++ src/imgs/Bubbles.tsx
@@ -5,7 +5,7 @@ import theme from "../theme";
 
 const PurpleBubble = () => (
   <ThoughtDook
-    style={{ marginRight: 8, marginLeft: 4, width: 24, height: 24 }}
+    style={{ marginRight: 8, marginLeft: 7, width: 24, height: 24 }}
     source={require("../../assets/pink/Dook.png")}
   />
 );
