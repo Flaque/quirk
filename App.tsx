24c9ae3543ee1ff904c9c35e91d021e88f81f6ea
Gurpreet <gurpreetsinghsangari13@gmail.com>
2021-04-10 13:34:44 +0530

    added comments

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { Platform } from "react-native";
 import { createAppContainer } from "react-navigation";
 import {
   EXPLANATION_SCREEN,
@@ -10,23 +12,22 @@ import {
   SUPPORT_SCREEN,
   MARKDOWN_ARTICLE_SCREEN,
 } from "./src/screens";
+import { setCustomText } from "react-native-global-props";
 import SettingScreen from "./src/SettingsScreen";
 import withErrorBoundary from "./src/sentry/withErrorBoundary";
 import PaymentScreen from "./src/payments/PaymentScreen";
 import LockScreen from "./src/lock/LockScreen";
 import MainScreen from "./src/main";
-import React from "react";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import TabBar from "./src/tabbar/TabBar";
 import OnboardingScreen from "./src/onboarding";
 import IndexLearnScreen from "./src/learn";
 import CheckupScreen from "./src/checkups";
-import { setCustomText } from "react-native-global-props";
-import { Platform } from "react-native";
 import SupportScreen from "./src/payments/SupportScreen";
 import MarkdownArticleScreen from "./src/articles/MarkdownArticleScreen";
 
 // Fixes a bug on OnePlus phones which have some buggy font by default
+//Bug Fixed
 if (Platform.OS === "android") {
   setCustomText({
     style: {
