081400487129185f95506866d30f9fef3043c511
Cosmicguard1an <cosmicguardian1@gmail.com>
2021-04-13 19:32:42 -0700

    Styling Changes: Font, size, color

--- App.tsx
+++ App.tsx
@@ -30,7 +30,9 @@ import MarkdownArticleScreen from "./src/articles/MarkdownArticleScreen";
 if (Platform.OS === "android") {
   setCustomText({
     style: {
-      fontFamily: "Roboto",
+      fontFamily: "Arial",
+      fontSize: 10,
+      fontColor: 'blue',
     },
   });
 }
