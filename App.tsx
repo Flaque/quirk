911982bd9af233aefac7c9008fb3c7bb5346d112
smanna1504 <smanna1504@gmail.com>
2021-04-29 15:12:47 +0530

    SUGGESTED SOME MODIFICATIONS IN App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,39 @@
-import { createAppContainer } from "react-navigation";
+import { createAppContainer/*createSwitchNavigator*/ } from "react-navigation";
+//import {StyleSheet,TouchableOpacity,Textinput,Button,Text,} from 'react-native'
+//import {Audio} from 'expo-av'
+//import {Header} from 'expo-font'
+//import * as Speech from 'expo-speech'
+ //In case you want to build your class:
+ /*export default class *class_Name* extends React.Component{
+  constructor(){
+    this.state={}
+  }
+  render(){
+    return(
+    )
+  }
+ }
+ */
+
+//In case you want to build your function:
+/*export default function *function_name*() */
+
+//In case you want to add your audio:
+/*Audio.Sound.createAsunc({
+  {uri:*mp3 link only*}
+  {shouldPlay:true}
+}) */
+
+//In case you want to add text-speech:
+//Speech.speak("*your words(even vars and states*")
+//To stop the speech:
+//Speech.pause()
+//If you paused it and you want to continue:
+//Speech.resume();
+
+
+
+
 import {
   EXPLANATION_SCREEN,
   SETTING_SCREEN,
