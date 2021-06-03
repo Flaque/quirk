b1c222906f3560c9139ffebbab87a89457b98078
ire2345 <gw14olowoyoireoluwa@glow.sch.uk>
2021-06-03 21:58:41 +0100

    I made the options look bigger

--- src/distortions.ts
+++ src/distortions.ts
@@ -29,54 +29,63 @@ const distortions: CognitiveDistortion[] = [
     label: i18n.t("all_or_nothing_thinking"),
     slug: "all-or-nothing",
     description: i18n.t("all_or_nothing_thinking_one_liner"),
+    size:50
   },
   {
     emoji: "👯‍",
     label: i18n.t("over_generalization"),
     slug: "overgeneralization",
     description: i18n.t("overgeneralization_one_liner"),
+    size:50
   },
   {
     emoji: emj("🧠", "💭"),
     label: i18n.t("mind_reading"),
     slug: "mind-reading",
     description: i18n.t("mind_reading_one_liner"),
+    size:50
   },
   {
     emoji: "🔮",
     label: i18n.t("fortune_telling"),
     slug: "fortune-telling",
     description: i18n.t("fortune_telling_one_liner"),
+    size:50
   },
   {
     emoji: "👎",
     label: i18n.t("magnification_of_the_negative"),
     slug: "magnification-of-the-negative",
     description: i18n.t("magnification_of_the_negative_one_liner"),
+    size:50
   },
   {
     emoji: "👍",
     label: i18n.t("minimization_of_the_positive"),
     slug: "minimization-of-the-positive",
     description: i18n.t("minimization_of_the_positive_one_liner"),
+    size:50
   },
   {
     emoji: emj("🤯", "💥"),
     label: i18n.t("catastrophizing"),
     slug: "catastrophizing",
     description: i18n.t("catastrophizing_one_liner"),
+    size:50
   },
   {
     emoji: "🎭",
     label: i18n.t("emotional_reasoning"),
     slug: "emotional-reasoning",
     description: i18n.t("emotional_reasoning_one_liner"),
+    size:50
   },
   {
     emoji: "✨",
     label: i18n.t("should_statements"),
     slug: "should-statements",
     description: i18n.t("should_statements_one_liner"),
+    size:50
   },
   {
     // ya know, because onigiri has like a little seaweed label.
@@ -85,6 +94,7 @@ const distortions: CognitiveDistortion[] = [
     label: i18n.t("labeling"),
     slug: "labeling",
     description: i18n.t("labeling_one_liner"),
+    size:50
   },
   {
     // look man don't ask me why it's a no-pedestrian as a fallback
@@ -93,12 +103,14 @@ const distortions: CognitiveDistortion[] = [
     label: i18n.t("self_blaming"),
     slug: "self-blaming",
     description: i18n.t("self_blaming_one_liner"),
+    size:50
   },
   {
     emoji: emj("🧛‍", "👺"),
     label: i18n.t("other_blaming"),
     slug: "other-blaming",
     description: i18n.t("other_blaming_one_liner"),
+    size:50
   },
 ].sort((first, second) => {
   const firstLabel = first.label.toUpperCase();
