d2b998bc8906e146fef01d74d47df0f7099bccd9
Aaryadeva <73352730+Aaryadeva@users.noreply.github.com>
2021-06-15 22:13:59 +0530

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -30,7 +30,8 @@ import MarkdownArticleScreen from "./src/articles/MarkdownArticleScreen";
 if (Platform.OS === "android") {
   setCustomText({
     style: {
-      fontFamily: "Roboto",
+      fontFamily: "Calibri",
+      fontSize:22
     },
   });
 }
