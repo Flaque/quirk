4ad9a5e217f692ed92454ce846c7ec417c21432e
Arnav <arnav.gupta1124@gmail.com>
2021-07-08 16:14:15 +0530

    modified

--- src/CBTView.tsx
+++ src/CBTView.tsx
@@ -12,7 +12,7 @@ const cognitiveDistortionsToText = cognitiveDistortions => {
       <Paragraph
         key={slug}
         style={{
-          marginBottom: 8,
+          marginBottom: 6,
         }}
       >
         {emojiForSlug(slug)} {label}
@@ -37,7 +37,7 @@ export default ({ thought }: ThoughtComponent) => (
       {thought.automaticThought ? (
         <BubbleThought
           style={{
-            marginTop: 0,
+            marginTop: 3,
           }}
         >
           {thought.automaticThought}
