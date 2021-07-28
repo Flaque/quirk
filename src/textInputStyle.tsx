861f81b2cf2a6673c8f1db8454a7cf643ffd8f7e
ShreyashSwarnkar <82525327+ShreyashSwarnkar@users.noreply.github.com>
2021-07-28 21:55:30 +0530

    added styling

--- src/textInputStyle.tsx
+++ src/textInputStyle.tsx
@@ -5,16 +5,16 @@ import { Dimensions, TextInput as ReactNativeTextInput } from "react-native";
 export const textInputStyle = {
   height: Dimensions.get("screen").height * 0.15,
   backgroundColor: "white",
+  backgroundColor: "gray",
   padding: 12,
   paddingTop: 14,
   borderRadius: 8,
   fontSize: 16,
+  fontSize: 18,
   borderColor: theme.lightGray,
   borderWidth: 1,
   borderBottomWidth: 2,
-  color: theme.darkText,
-  textAlignVertical: "top",
-};
+},
 export const textInputPlaceholderColor = theme.veryLightText;
 
 export const TextInput = (props: {
