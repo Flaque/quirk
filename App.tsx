a507a20fb4a2477e7f24a97ce18f15bd87a3f568
Asmit-Bhattacharjee <Bhattacharjee.amlan@gmail.com>
2021-08-11 23:02:29 +0530

    modified some styles

--- App.tsx
+++ App.tsx
@@ -30,7 +30,9 @@ import MarkdownArticleScreen from "./src/articles/MarkdownArticleScreen";
 if (Platform.OS === "android") {
   setCustomText({
     style: {
-      fontFamily: "Roboto",
+      fontFamily: "Monospace",
+      color:"blue",
+      backgroundColor:"red",
     },
   });
 }
