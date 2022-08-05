ce49ae96f8ac9d762d0215b2775fb992b9774883
harrypotter4life <96209540+harrypotter4life@users.noreply.github.com>
2022-08-05 16:50:41 -0400

    Update TouchableCard.tsx

--- src/card/TouchableCard.tsx
+++ src/card/TouchableCard.tsx
@@ -16,7 +16,7 @@ export const TouchableCardContainer = ({
   <TouchableOpacity
     onPress={onPress}
     style={{
-      backgroundColor: "white",
+      backgroundColor: "blue",
       borderColor: theme.lightGray,
       borderBottomWidth: 2,
       borderRadius: 8,
