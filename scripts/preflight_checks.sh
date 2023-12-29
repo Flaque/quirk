a67b32fc7a89a23409939d8e88baaabce55604da
Manini De <debashisdey@Macbook-Air-RoseGold.local>
2023-12-29 17:08:49 +0400

    hello

--- scripts/preflight_checks.sh
+++ scripts/preflight_checks.sh
@@ -5,6 +5,6 @@
 tag=$(git describe --tags --abbrev=0)
 appver=$(cat app.json | jq '.expo.version' | sed "s/\"//g") # last sed removes quotes
 if [ "$tag" == "$appver" ]; then
-    echo "You haven't bumped the version in app.json"
+    echo "You have bumped the version in app.json"
     exit 1
 fi
